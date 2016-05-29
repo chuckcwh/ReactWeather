05608fdf387fac085c9e8429cbd6cfc4aee7553d
chuckcwh <chuckcwh@gmail.com>
2016-05-29 23:13:51 +0800

    Style about page & add content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,19 @@ var React = require('react');
 //if only simple render function, use stateless functional component instead
 var About = () => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build on React. It uses Open Weather API to find the current temperature of the location you input.</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - This is used to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   )
 };
 
