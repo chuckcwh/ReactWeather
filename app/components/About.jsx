0512052b7e3f0d5a6ba3319fca1bf3cac5241953
chuckcwh <chuckcwh@gmail.com>
2016-05-29 18:46:32 +0800

    add foundation & jquery

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,14 +1,5 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function() {
-//     return (
-//       <h3>About Component</h3>
-//     )
-//   }
-// });
-
-
 //if only simple render function, use stateless functional component instead
 var About = () => {
   return (
